dd2e4ba9730ba80f5baed44f0de95136d12c1a82
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-08-04 15:16:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let login = document.querySelector(".login");
 let signup = document.querySelector(".signup");
 
 login.addEventListener("click",()=>{
-    location.href = "../login";
+    location.href = "./login/login.html";
 })
 
 signup.addEventListener('click',()=>{
-    location.href = '../signup';
+    location.href = './signup/signup.html';
 })
